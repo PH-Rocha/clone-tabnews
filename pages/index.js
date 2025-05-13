593fa0be52c5691a06ced12051fa8cd1b4dae66e
Pedro Rocha <phr123silva@gmail.com>
2025-05-13 10:37:02 -0300

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Feliz dia das Mães! S2</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
